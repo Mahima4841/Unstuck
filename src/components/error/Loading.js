ca87ebb9b36043e5a58771e9264bab29f37d0415
Mahima4841 <mahima4841@gmail.com>
2023-08-20 20:03:14 +0530

    responsiveness, UI improvement

---
+++ src/components/error/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import { Container, Heading } from '../fragments/mainLayout';
+
+function Loading(){
+    return(
+        <Container>
+            <h2>
+            <center>
+                Loading...
+            </center>
+            </h2>
+        </Container>
+    )
+}
+
+export default Loading ;
